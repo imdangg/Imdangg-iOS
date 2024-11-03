7527b7ebe5c8a7ec536efa0d0797931f2adcdd3d
DAEJINLIM <daejin.limm@gmail.com>
2024-11-03 17:54:33 +0900

    Feat: 폰트 유틸 추가, 라이브러리 추가

---
+++ Imdangg/Utils/FontUtility.swift
@@ -0,0 +1,30 @@
+//
+//  FontUtility.swift
+//  Imdangg
+//
+//  Created by 임대진 on 11/3/24.
+//
+
+import UIKit
+
+final class FontUtility {
+    enum PretendardVariable: String {
+        case regular = "PretendardVariable-Regular"
+        case thin = "PretendardVariable-Thin"
+        case extraLight = "PretendardVariable-ExtraLight"
+        case light = "PretendardVariable-Light"
+        case medium = "PretendardVariable-Medium"
+        case semiBold = "PretendardVariable-SemiBold"
+        case bold = "PretendardVariable-Bold"
+        case extraBold = "PretendardVariable-ExtraBold"
+        case black = "PretendardVariable-Black"
+        
+        func fonr(size: CGFloat) -> UIFont? {
+            return UIFont(name: self.rawValue, size: size)
+        }
+    }
+    
+    static func getFonr(type: PretendardVariable, size: CGFloat) -> UIFont? {
+        return type.fonr(size: size)
+    }
+}
