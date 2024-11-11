84818ebc0fd8c33599c67c1108d5cc6743f68af1
DAEJINLIM <daejin.limm@gmail.com>
2024-11-11 23:10:54 +0900

    rename: gg -> g 수정

--- imdang/Tests/ImdangTests.swift
+++ imdang/Tests/ImdangTests.swift
@@ -1,7 +1,7 @@
 import Foundation
 import XCTest
 
-final class ImdanggTests: XCTestCase {
+final class ImdangTests: XCTestCase {
     func test_twoPlusTwo_isFour() {
         XCTAssertEqual(2+2, 4)
     }
