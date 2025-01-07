3c07bb82fbd5d6922cffdb69eaca3394201e9537
DAEJINLIM <daejin.limm@gmail.com>
2025-01-07 21:48:12 +0900

    Fix: textLineHeight 로직 변경 및 레이아웃 수정

--- Projects/imdang/Sources/Utils/Extensions/UILabel +.swift
+++ Projects/imdang/Sources/Utils/Extensions/UILabel +.swift
@@ -8,30 +8,48 @@
 import UIKit
 
 extension UILabel {
-    /// 고정값 간격
-    func setLineSpacing(spacing: CGFloat) {
-        guard let text = text else { return }
-
-        let attributeString = NSMutableAttributedString(string: text)
-        let style = NSMutableParagraphStyle()
-        style.lineSpacing = spacing
-        attributeString.addAttribute(.paragraphStyle,
-                                     value: style,
-                                     range: NSRange(location: 0, length: attributeString.length))
-        attributedText = attributeString
-    }
+//    /// 고정값 간격
+//    func setLineSpacing(spacing: CGFloat) {
+//        guard let text = text else { return }
+//
+//        let attributeString = NSMutableAttributedString(string: text)
+//        let style = NSMutableParagraphStyle()
+//        style.lineSpacing = spacing
+//        attributeString.addAttribute(.paragraphStyle,
+//                                     value: style,
+//                                     range: NSRange(location: 0, length: attributeString.length))
+//        attributedText = attributeString
+//    }
+//    
+//    /// 비율 간격
+//    func setLineSpacing(lineHeightMultiple: CGFloat) {
+//        guard let text = self.text else { return }
+//        
+//        let paragraphStyle = NSMutableParagraphStyle()
+//        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+//        
+//        let attributedString = NSAttributedString(string: text, attributes: [
+//            .paragraphStyle: paragraphStyle
+//        ])
+//        
+//        self.attributedText = attributedString
+//    }
     
-    /// 비율 간격
-    func setLineSpacing(lineHeightMultiple: CGFloat) {
-        guard let text = self.text else { return }
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineHeightMultiple = lineHeightMultiple
-        
-        let attributedString = NSAttributedString(string: text, attributes: [
-            .paragraphStyle: paragraphStyle
-        ])
-        
-        self.attributedText = attributedString
+    /// 피그마 Line height 그대로 넣으면 적용됨
+    func setTextWithLineHeight(text: String?, lineHeight: CGFloat) {
+        if let text = text {
+            let style = NSMutableParagraphStyle()
+            style.maximumLineHeight = lineHeight
+            style.minimumLineHeight = lineHeight
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .paragraphStyle: style,
+                .baselineOffset: (lineHeight - font.lineHeight) / 4
+            ]
+            
+            let attrString = NSAttributedString(string: text,
+                                                attributes: attributes)
+            self.attributedText = attrString
+        }
     }
 }
