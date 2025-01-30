6404da9cdf73a840952dc1dda05ab32acc82f065
da-hye0 <60743139+da-hye0@users.noreply.github.com>
2025-01-30 16:18:49 +0900

    Feat: 애플로그인 추가

--- Projects/imdang/Sources/Services/AppleLoginService.swift
+++ Projects/imdang/Sources/Services/AppleLoginService.swift
@@ -4,14 +4,18 @@
 //
 //  Created by daye on 11/14/24.
 //
+
 import Foundation
 import AuthenticationServices
 import RxSwift
-
+import NetworkKit
 
 class AppleLoginService: NSObject, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
     
+    private let networkManager = NetworkManager()
     static let shared = AppleLoginService()
+    let disposebag = DisposeBag()
+    
     let loginResult = PublishSubject<Result<ASAuthorizationAppleIDCredential, Error>>()
     
     func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
@@ -31,22 +35,35 @@ class AppleLoginService: NSObject, ASAuthorizationControllerDelegate, ASAuthoriz
         controller.presentationContextProvider = self
         controller.performRequests()
     }
-
+    
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
-            
-            let userIdentifier = appleIDCredential.user
-            let fullName = appleIDCredential.fullName
-            let email = appleIDCredential.email
-            
-            // user info
-            print("사용자 ID: \(userIdentifier)\n")
-            print("사용자 name: \(String(describing: fullName))\n")
-            print("사용자 email: \(String(describing: email))\n")
-            
-            loginResult.onNext(.success(appleIDCredential))
+            if let authorizationCode = appleIDCredential.authorizationCode,
+               let code = String(data: authorizationCode, encoding: .utf8) {
+                print("Authorization Code: \(code)")
+                
+                // 서버로 authorizationCode 전송
+                sendAppleInfoToServer(authorizationCode: code)
+                    .subscribe(
+                        onNext: { success in
+                            if success {
+                                print("Apple 로그인 성공: 사용자 정보가 서버에 저장되었습니다.")
+                                self.loginResult.onNext(.success(appleIDCredential))
+                            } else {
+                                print("Apple 로그인 실패: 서버 처리 중 문제가 발생했습니다.")
+                                self.loginResult.onNext(.failure(NSError(domain: "AppleLogin", code: -1, userInfo: [NSLocalizedDescriptionKey: "서버 처리 실패"])))
+                            }
+                        },
+                        onError: { error in
+                            print("Apple 로그인 네트워크 에러: \(error.localizedDescription)")
+                            self.loginResult.onNext(.failure(error))
+                        }
+                    )
+                    .disposed(by: disposebag)
+            }
         }
     }
+
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         let nsError = error as NSError
@@ -56,8 +73,75 @@ class AppleLoginService: NSObject, ASAuthorizationControllerDelegate, ASAuthoriz
                 print("Apple Login Error: \(error.localizedDescription)")
                 loginResult.onNext(.failure(error))
             }
-        
+    }
+}
+
+extension AppleLoginService {
+    func sendAppleInfoToServer(authorizationCode: String) -> Observable<Bool> {
+        return Observable.create { [self] observer in
+            
+            let parameters: [String: Any] = [
+                "authorizationCode": authorizationCode
+            ]
+            let endpoint = Endpoint<User>(
+                baseURL: .imdangAPI,
+                path: "/auth/apple",
+                method: .post,
+                parameters: parameters
+            )
+            
+            networkManager.request(with: endpoint)
+                .subscribe(
+                    onNext: { response in
+                        UserdefaultKey.accessToken = response.accessToken
+                        UserdefaultKey.memberId = response.memberId
+
+                        observer.onNext(true)
+                        observer.onCompleted()
+                    },
+                    onError: { error in
+                        print("Request failed with error: \(error)")
+
+                        observer.onNext(false)
+                        observer.onCompleted()
+                    }
+                )
+                .disposed(by: disposebag)
+            
+            return Disposables.create()
+        }
     }
     
-    
+    func sendAppleWithdrawalToServer(refreshToken: String) -> Observable<Bool> {
+        return Observable.create { [self] observer in
+            
+            let parameters: [String: Any] = [
+                "token": refreshToken
+            ]
+            
+            let endpoint = Endpoint<User>(
+                baseURL: .imdangAPI,
+                path: "/members/withdrawal/apple",
+                method: .post,
+                parameters: parameters
+            )
+            
+            networkManager.request(with: endpoint)
+                .subscribe(
+                    onNext: { response in
+                        observer.onNext(true)
+                        observer.onCompleted()
+                    },
+                    onError: { error in
+                        print("Request failed with error: \(error)")
+
+                        observer.onNext(false)
+                        observer.onCompleted()
+                    }
+                )
+                .disposed(by: disposebag)
+            
+            return Disposables.create()
+        }
+    }
 }
